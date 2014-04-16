583a7d64064985285be04b5afb24c9cfbac76ef3
aj3 <aj3@31b05262-34a9-4923-903e-e8e313387d9b>
2014-04-16 16:52:01 +0000

    SKGB-offline: v0.2.6
    git-svn-id: svn+ssh://cat/usr/local/svnreps/skgb/skgb-offline/trunk@664 31b05262-34a9-4923-903e-e8e313387d9b

--- de/skgb/offline/gui/Gui.java
+++ de/skgb/offline/gui/Gui.java
@@ -27,7 +27,7 @@ class Gui implements ActionListener {
 	 * package. At some point this field should be moved over there so that
 	 * we have a grand unified version number for the whole project.
 	 */
-	static final String version = "0.2.5";
+	static final String version = "0.2.6";
 	
 	// used for stack trace abbreviation
 	private static final String myPackageLeader = "de.skgb.";
